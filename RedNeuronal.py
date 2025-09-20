82d1b46da9c4e42adbce7e35bf5d71b2dfaab75b
valeAlfaro5 <valeriasalfaro@gmail.com>
2025-09-19 21:37:18 -0600

    ejercicio4 resuelto, avance del 5

--- RedNeuronal.py
+++ RedNeuronal.py
@@ -2,7 +2,6 @@
 import numpy as np
 import DnnLib
 import json
-import random
 import matplotlib.pyplot as plt
 
 # %%
@@ -85,25 +84,54 @@ outs = layer2.forward(salidas)
 # %%
 # Para test
 predicts = np.argmax(sal, axis=1)
-acurr = np.mean(predicts == labels)   # (test)
+acurr = np.mean(predicts == labels)
 print("Predicción test:", acurr * 100)
 
 # Para entrenamiento
 predicts = np.argmax(outs, axis=1)
-acurr = np.mean(predicts == label)    #   (train)
+acurr = np.mean(predicts == label)   
 print("Predicción entrenamiento:", acurr * 100)
 
 
 # %%
+#entrenamiento de red neuronal - parte 4
+#creando arreglos & variables necesarias
+
+capa1 = DnnLib.DenseLayer(784, 128, DnnLib.ActivationType.RELU)
+
+#segunda capa tiene 128 entradas(de capa 1) y 10 de salida, activacion softmax
+capa2 = DnnLib.DenseLayer(128, 10, DnnLib.ActivationType.SOFTMAX)
+
+capa = [capa1, capa2]
+
+#arreglo de optimizadores
+optimizers = DnnLib.Adam(0.001)
+
+pruebas = np.load("mnist_train.npz")
+image = pruebas["images"]
+label = pruebas["labels"]
+
+f = image.reshape(-1, 784).astype(np.float32) / 255
+
+epochs = 10
+
+#creación de one hot
+n = label.shape[0]  
+y = np.zeros((n, 10), dtype=np.float64)
+y[np.arange(n), label] = 1.0
+
+
 #arreglo de layers ya definidio, arreglo de todos los optimizadores, f son las entradas del mnist-train, y es el one hot, label a lo que estoy comparando y epochs epocas predefinidas, batch sizes para que pueda correr sin tronar 
-def entrenamiento1(capas, optimizer, f, y_onehot, label, epochs=50, batch_size=128):
+def entrenamiento1(capas, optimizer, f, y_onehot, label, epochs, batch_size=128):
     n = f.shape[0]
     for e in range(epochs):
-        # barajar los datos
         indices = np.random.permutation(n)
         f, y_onehot, label = f[indices], y_onehot[indices], label[indices]
 
-        # mini-batches
+        # datos para calcular promedio de loss y accuracy
+        loss_epoca = 0.0
+        nb, correct, total = 0, 0, 0
+
         for i in range(0, n, batch_size):
             X_batch = f[i:i+batch_size]
             y_batch = y_onehot[i:i+batch_size]
@@ -113,55 +141,75 @@ def entrenamiento1(capas, optimizer, f, y_onehot, label, epochs=50, batch_size=1
             out1 = capas[0].forward(X_batch)
             out2 = capas[1].forward(out1)
 
-            # backward y update
+            # loss
+            perdida = DnnLib.cross_entropy(out2, y_batch)
+
+            # backward
             grad = DnnLib.cross_entropy_gradient(out2, y_batch)
             grad = capas[1].backward(grad)
             grad = capas[0].backward(grad)
 
+            # actualización con Adam
             optimizer.update(capas[1])
             optimizer.update(capas[0])
 
-        # imprimir cada 10 épocas
-        if e % 10 == 0:
+            # acumular métricas
+            loss_epoca += perdida
+            nb += 1
+
             preds = np.argmax(out2, axis=1)
-            acc = np.mean(preds == label_batch)
-            print(f"Epoch {e}, Loss: {DnnLib.cross_entropy(out2, y_batch):.4f}, Acc: {acc:.4f}")
+            correct += np.sum(preds == label_batch)
+            total += len(label_batch)
+            
+            # if i % 50 == 0:  # cada 50 batches
+            #     print(f"Batch {i}, Loss: {perdida:.4f}, Accuracy: {correct}")
 
+        # promedios por época
+        avg_loss = loss_epoca / nb
+        acc = correct / total
 
-# %%
-#entrenamiento de red neuronal - parte 4
-#creando arreglos & variables necesarias
+        print(f"Epoch {e+1}/{epochs}, Loss: {avg_loss:.4f}, Acc: {acc*100:.2f}%")
 
-capa1 = DnnLib.DenseLayer(784, 128, DnnLib.ActivationType.RELU)
-#segunda capa tiene 128 entradas(de capa 1) y 10 de salida, activacion softmax
-capa2 = DnnLib.DenseLayer(128, 10, DnnLib.ActivationType.SOFTMAX)
-
-capa1.weights = np.random.randn(128,784)*0.01
-capa1.bias = np.zeros(128,)
-capa2.weights = np.random.randn(10, 128)*0.01
-capa2.bias = np.zeros(10,)
-
-capa = [capa1, capa2]
-
-#arreglo de optimizadores
-optimizers = [
-    ("Adam", DnnLib.Adam(0.001))
-]
-
-epochs = 100
-
-#creación de one hot
-n = label.shape[0]  
-y = np.zeros((n, 10), dtype=np.float64)
-y[np.arange(n), label] = 1.0
 
 
 
 # %%
 #llamada de función de entrenamiento
-per = []
-per = entrenamiento1(capa, optimizers[0][1], f, y, label, epochs)
-print(per)
+entrenamiento1(capa, optimizers, f, y, label, epochs)
+
+# %%
+def cargar(capas):
+    modelo = {
+        "input_shape": [28, 28],
+        "preprocess": {
+            "scale": 255
+        },
+        "layers": []
+    }
+    layer1= {
+        "type": "dense",
+        "units": 128,
+        "activation": "relu",
+        "W": capas[0].weights.tolist(),
+        "b": capas[0].bias.tolist()
+    }
+    layer2 = {
+        "type": "dense",
+        "units": 10,
+        "activation": "softmax",
+        "W": capas[1].weights.tolist(),
+        "b": capas[0].bias.tolist()
+    }
+    
+
+    modelo["layers"].append(layer1)
+    modelo["layers"].append(layer2)
+    
+    with open("mnist_entrenado.json", "w") as f:
+        json.dump(modelo, f)
+
+cargar(capa)
+        
 
 # %%
 
